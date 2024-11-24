b9229161422bfa63f6ee0c94c2b7b50a265aefc4
tticehurst <46826453+tticehurst@users.noreply.github.com>
2024-11-24 11:39:34 +0000

    caprover

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,9 @@
 <?php
-return new PDO("mysql:host=localhost;dbname=website", "root", "", [
+$host = getenv('MYSQL_HOST');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+
+return new PDO("mysql:host=$host;dbname=website", "$username", "$password", [
   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   PDO::ATTR_EMULATE_PREPARES   => false,
